93b742268808fffd8b9155631c21626b25b3e4a1
vidya310 <117831004+vidya310@users.noreply.github.com>
2023-09-29 15:31:31 +0530

    Create boolean.py

---
+++ review of python pgm/boolean.py
@@ -0,0 +1,11 @@
+print("Demo of basic data types:Boolean")
+t=True
+f=False
+print("t=",t)
+print("f=",f)
+print("Data type of variable t:",type(t))
+print("Data type of variable f:",type(f))
+print("Logical AND operation:",t and f)
+print("Logical OR operation:",t or f)
+print("Logical NOT operation:",not t)
+print("Logical XOR operation:",t != f)
