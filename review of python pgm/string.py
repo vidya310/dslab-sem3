30026d620ac7b82d6c68201835c3091ca19f9dfe
vidya310 <117831004+vidya310@users.noreply.github.com>
2023-09-29 15:32:37 +0530

    Create string.py

---
+++ review of python pgm/string.py
@@ -0,0 +1,12 @@
+print("Demo of basic data types:String")
+s="hello"
+t="world"
+print("string1:",s)
+print("string2:",t)
+d=s+" "+t
+print("String concatenation:",d)
+print("Capitalize:",d.capitalize())
+print("Converted to upper case:",s.upper())
+print("Right justify a string:",s.rjust(7))
+print("After replacing l with ell:",s.replace('l','(ell)'))
+print("String after striping leading and trailing white spaces:",'world'.strip())
