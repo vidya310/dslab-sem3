639501975a869da9abf0382a8e6ba44bf23a1c81
vidya310 <117831004+vidya310@users.noreply.github.com>
2023-09-29 14:33:54 +0530

    Create datatype.py

---
+++ review of python pgm/datatype.py
@@ -0,0 +1,11 @@
+print("Demo of basic data types:Numbers")
+x=3
+y=2.5
+print("x=",x)
+print("y=",y)
+print("Data type of variable x:",type(x))
+print("Data type of variable y:",type(y))
+print("Addition:",x+y)
+print("Subtraction:",x-y)
+print("Multiplication:",x*2)
+print("Exponentiation:",x**2)
